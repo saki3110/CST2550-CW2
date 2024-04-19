233bc1ff67ab76d11d6ccf9aaf9ba8fb5e6adf08
sakina3110 <129828522+sakina3110@users.noreply.github.com>
2024-04-19 16:46:44 +0400

    Implementing getters

--- Customer.cpp
+++ Customer.cpp
@@ -4,4 +4,29 @@
 
 #include "Customer.h"
 
+Customer::Customer(int id, const std::string& name, const std::string& address, const std::string& phoneNumber, const std::string& email)
+        : id(id), name(name), address(address), phoneNumber(phoneNumber), email(email), membershipStatus("Regular") {}
+
+int Customer::getId() const {
+    return id;
+}
+
+std::string Customer::getName() const {
+    return name;
+}
+
+std::string Customer::getAddress() const {
+    return address;
+}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+std::string Customer::getPhoneNumber() const {
+    return phoneNumber;
+}
+
+std::string Customer::getEmail() const {
+    return email;
+}
+
+std::string Customer::getMembershipStatus() const {
+    return membershipStatus;
 }

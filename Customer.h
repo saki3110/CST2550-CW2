ac54b656c4ad8bcf4ef79738002e1122cc1fd410
sakina3110 <129828522+sakina3110@users.noreply.github.com>
2024-04-19 13:12:36 +0400

    Header file for Customer created

--- Customer.h
+++ Customer.h
@@ -5,4 +5,11 @@
 #ifndef CST2550_CW2_CUSTOMER_H
 #define CST2550_CW2_CUSTOMER_H
 
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <unordered_set>
+
 #endif //CST2550_CW2_CUSTOMER_H

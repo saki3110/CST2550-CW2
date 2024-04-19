af5cbc2e4ccda9a7aba3cb937b28160cae651414
Yashvin Balloo <NB1070@live.mdx.ac.uk>
2024-04-19 14:11:09 +0400

    created public and private class for rental

---
+++ rental.h
@@ -0,0 +1,30 @@
+#ifndef RENTAL_H
+#define RENTAL_H
+
+#include <ctime>
+#include <string>
+
+class Rental {
+private:
+    int id;
+    int movieId;
+    int customerId;
+    std::time_t rentalDate;
+    std::time_t dueDate;
+    std::time_t returnDate;
+    double rentalFee;
+
+public:
+    Rental(int id, int movieId, int customerId, const std::time_t& rentalDate, const std::time_t& dueDate, double rentalFee);
+    int getId() const;
+    int getMovieId() const;
+    int getCustomerId() const;
+    std::time_t getRentalDate() const;
+    std::time_t getDueDate() const;
+    std::time_t getReturnDate() const;
+    double getRentalFee() const;
+    double calculateLateFee(const std::time_t& returnDate) const;
+    void setReturnDate(const std::time_t& returnDate);
+};
+
+#endif

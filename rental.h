8714a9004f7e764565b5bb24afcc165d8246ce57
Yashvin Balloo <NB1070@live.mdx.ac.uk>
2024-04-19 14:23:02 +0400

    Added comments to Rental class implementation and header file

--- rental.h
+++ rental.h
@@ -6,13 +6,13 @@
 
 class Rental {
 private:
-    int id;
-    int movieId;
-    int customerId;
-    std::time_t rentalDate;
-    std::time_t dueDate;
-    std::time_t returnDate;
-    double rentalFee;
+    int id; // Unique identifier for the rental
+    int movieId; // Identifier of the rented movie
+    int customerId; // Identifier of the customer who rented the movie
+    std::time_t rentalDate; // Date when the rental was made
+    std::time_t dueDate; // Date when the rented movie is due to be returned
+    std::time_t returnDate; // Date when the rented movie was returned
+    double rentalFee; // Fee charged for the rental
 
 public:
     Rental(int id, int movieId, int customerId, const std::time_t& rentalDate, const std::time_t& dueDate, double rentalFee);

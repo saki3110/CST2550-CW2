2da83186b0c2d8d375558f80b19a943559876e3e
sakina3110 <129828522+sakina3110@users.noreply.github.com>
2024-04-19 16:12:43 +0400

    Implementing classes and functionalities

--- Customer.h
+++ Customer.h
@@ -15,10 +15,28 @@
 class Customer {
 private:
     int id;
+    std::string name;
+    std::string address;
+    std::string phoneNumber;
+    std::string email;
+    std::string membershipStatus;
 
 public:
+    Customer(int id, const std::string& name, const std::string& address, const std::string& phoneNumber, const std::string& email);
     int getId() const;
-
+    std::string getName() const;
+    std::string getAddress() const;
+    std::string getPhoneNumber() const;
+    std::string getEmail() const;
+    std::string getMembershipStatus() const;
+    void updateMembershipStatus(const std::string& newStatus);
 };
+void displayAllCustomers(const std::vector<Customer>& customers);
+void displayCustomerWithID(const std::vector<Customer>& customers, int id);
+void displayCustomerWithName(const std::vector<Customer>& customers, const std::string& name);
+void addNewCustomer(std::vector<Customer>& customers);
+void updateMembershipStatus(std::vector<Customer>& customers);
+void displayAllRegularCustomers(const std::vector<Customer>& customers);
+void displayAllPremiumCustomers(const std::vector<Customer>& customers);
 
 #endif //CST2550_CW2_CUSTOMER_H

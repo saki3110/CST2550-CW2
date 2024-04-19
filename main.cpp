555b52912a9278b040c3256cc33804a32e56e288
sakina3110 <129828522+sakina3110@users.noreply.github.com>
2024-04-19 13:17:39 +0400

    Libraries and other files imported

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <unordered_set>
+#include "Movie.h"
+#include "Customer.h"
+#include "Rental.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;

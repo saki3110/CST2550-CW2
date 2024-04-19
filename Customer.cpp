1328c2ff84487608b78bb9fcef5f4253f73aa8df
sakina3110 <129828522+sakina3110@users.noreply.github.com>
2024-04-19 16:49:57 +0400

    Functions to display customer details added

--- Customer.cpp
+++ Customer.cpp
@@ -30,3 +30,60 @@ std::string Customer::getEmail() const {
 std::string Customer::getMembershipStatus() const {
     return membershipStatus;
 }
+
+oid Customer::updateMembershipStatus(const std::string& newStatus) {
+    membershipStatus = newStatus;
+}
+
+void displayAllCustomers(const std::vector<Customer>& customers) {
+    std::cout << "Customers:\n\n";
+    for (const auto& customer : customers) {
+        std::cout << "ID: " << customer.getId() << "\nName: " << customer.getName() << "\nAddress: " << customer.getAddress()
+                  << "\nPhone Number: " << customer.getPhoneNumber() << "\nEmail: " << customer.getEmail()
+                  << "\nMembership Status: " << customer.getMembershipStatus() << "\n\n";
+    }
+}
+
+void displayCustomerWithID(const std::vector<Customer>& customers, int id) {
+    for (const auto& customer : customers) {
+        if (customer.getId() == id) {
+            std::cout << "ID: " << customer.getId() << "\nName: " << customer.getName() << "\nAddress: " << customer.getAddress()
+                      << "\nPhone Number: " << customer.getPhoneNumber() << "\nEmail: " << customer.getEmail()
+                      << "\nMembership Status: " << customer.getMembershipStatus() << "\n\n";
+        }
+    }
+    if (std::find_if(customers.begin(), customers.end(), [id](const Customer& customer) { return customer.getId() == id; }) == customers.end()) {
+        std::cout << "No customer found with this ID.\n";
+    }
+}
+
+void displayCustomerWithName(const std::vector<Customer>& customers, const std::string& name) {
+    for (const auto& customer : customers) {
+        if (customer.getName() == name) {
+            std::cout << "ID: " << customer.getId() << "\nName: " << customer.getName() << "\nAddress: " << customer.getAddress()
+                      << "\nPhone Number: " << customer.getPhoneNumber() << "\nEmail: " << customer.getEmail()
+                      << "\nMembership Status: " << customer.getMembershipStatus() << "\n\n";
+        }
+    }
+    if (std::find_if(customers.begin(), customers.end(), [name](const Customer& customer) { return customer.getName() == name; }) == customers.end()) {
+        std::cout << "No customer found with this name.\n";
+    }
+}
+
+void addNewCustomer(std::vector<Customer>& customers) {
+    int id = customers.size() + 1;
+    std::string name, address, phoneNumber, email;
+    char membershipOption;
+
+    std::cout << "Enter customer name: ";
+    std::cin.ignore();
+    std::getline(std::cin, name);
+    std::cout << "Enter customer address: ";
+    std::getline(std::cin, address);
+    std::cout << "Enter customer phone number: ";
+    std::cin >> phoneNumber;
+    std::cout << "Enter customer email: ";
+    std::cin >> email;
+
+}
+
